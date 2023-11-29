ee45df99c1dd46968e6b16fc7394d1ca161b33b0
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2023-11-29 08:57:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ if submit_button:
         
         LLMresponse = response
         st.write("Here is the JSON for the chart generation:")
-        st.json(LLMresponse)
+        #st.json(LLMresponse)
     
         #print(response.status_code)
         #print(response.json())
